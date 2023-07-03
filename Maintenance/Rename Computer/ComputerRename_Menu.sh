efa33745d00b5f15cdba82f117b559d890cf2c04
Zac <129307974+archzaq@users.noreply.github.com>
2023-07-02 22:31:17 -0500

    Added Lab Device support

--- Maintenance/Rename Computer/ComputerRename_Menu.sh
+++ Maintenance/Rename Computer/ComputerRename_Menu.sh
@@ -9,10 +9,7 @@ currentName=$(hostname)
 computerSerial=$(ioreg -l | grep IOPlatformSerialNumber | sed 's/"$//' | sed 's/.*"//')
 serialShort=${computerSerial: -6}
 
-# SLU standard naming scheme
-standardName="SLU-$serialShort"
-
-# Function to avoid repeating the scutil commands
+# Contains scutil commands
 function rename_Device() {
     /usr/sbin/scutil --set ComputerName $1
 	/usr/sbin/scutil --set LocalHostName $1
@@ -20,37 +17,100 @@ function rename_Device() {
 	/usr/local/bin/jamf recon	
 }
 
-# Function to prompt user to choose device department prefix
+# Prompts user to choose device department prefix
 function department_Prompt() {
 	echo "Prompting user for department..."
 	department=$(osascript <<YOO
 		set dropdownResult to choose from list \
-		{"1818", "AAS", "AAF", "ADM", "ANS", "AT", "BF", "BIO", "BIOC", "CADE",\
-		"C4", "CFS", "CCO", "CHCE", "CHM", "CME", "COMPMED", "CSB", "CTO",\
-		"DPS", "DRM", "DUR", "EAS", "EM", "ENG", "EU", "EVT", "FAC", "FCM",\
-		"FHS", "GME", "GC", "HIS", "HER", "HR", "IPE", "IM", "IM-GI", "INTO",\
-		"ITS", "LIB", "LAW", "MAR", "MED", "MED-ADM", "MM", "MMI", "MCL", "MCS",\
-		"MOC", "NEU", "OB", "OPT", "ORT", "OTO", "PAR", "PAT", "PED", "PHARM",\
-		"PHY", "POL", "PO", "PSY", "RAD", "REG", "RES", "SCJ", "SLUCOR", "SOE",\
-		"SON", "SPH", "SPS", "SDEV", "SUR", "SW", "THE", "WMS"}\
+		{"1818 - 1818 Program", "AAS - African-American Studies", "AAF - Academic Affairs",\
+		"ADM - Administration Arts and Sciences", "AT - Athletics", "BF - Business & Finance",\
+		"BIO - Biology", "BIOC - Biochemistry", "CADE - Center for Advanced Dental Education",\
+		"CFS - Center for Sustainability", "CHCE - Center for HealthCare Ethics", "CHM - Chemistry",\
+		"CME - Continuing Medical Education(PAWS)", "COMPMED - Comparative Medicine ",\
+		"CSB - Cook School of Business", "CTO - Clinical Trial Office",\
+		"DPS - Department of Public Safety", "DUR - University Development",\
+		"EAS - Earth and Atmospheric Science", "EM - Enrollment Management", "ENG - English",\
+		"EU - Clinical Skill", "EVT - Event Services", "FAC - Facilities",\
+		"GC - Office of General Counsel", "HIS - History", "HR - Human Resources",\
+		"IPE - Interprofessional Education Program", "IM-GI - GI-Research", "INTO - INTO_SLU",\
+		"ITS - Information Technology Services", "Lab Device", "LIB - Libraries", "LAW - School of Law",\
+		"MAR - Marketing & Communications", "MED - Medical School", "MM - Mission & Ministry",\
+		"MMI - Molecular Microbiology and Immunology", "MCL - Language Literature and Cultures",\
+		"MCS - Math and Mathematical Computer Science", "MOC - Museum of Contemporary Religious Art",\
+		"PAR - Parks College", "PHARM - Pharmacology and Physiology",\
+		"PHY - Physics", "POL - Political Science", "PO - President's Office",\
+		"REG - Office of the Registrar", "RES - Research Admin", "SCJ - Sociology and Anthropology",\
+		"SLUCOR - SLU Center of Outcomes Research", "SOE - School of Education",\
+		"SON - School of Nursing", "SPH - School of Public Health",\
+		"SPS - School of Professional Studies", "SDEV - Student Development", "SW - Social Work",\
+		"THE - Theological Studies", "WMS - Women’s Studies Program"}\
 		with title "SLU ITS: Device Rename" with prompt "Please choose your department:"
 		return dropdownResult
 YOO
-)
-	dept=$(echo "$department-")
+	)
+	# sed command to only grab the text before the ' - ' in the list options
+	dept=$(echo "$department" | sed 's/\(.*\) - .*/\1/' | sed 's/[[:space:]]*$//')
+	# Cancels the operation if the user selects Cancel from the osascript prompt
 	if [[ "$dept" == *"false"* ]];
 	then
 		echo "User canceled the operation."
-		exit 0
+		exit 1
+	# Run the lab_Prompt function for lab devices
+	elif [[ "echo $department" == *"Lab"* ]];
+	then
+		lab_Prompt # lab prompt function
+	# Anyone that didnt cancel or select lab device, rename using chosen option
+	else
+		deptName="${dept}-${serialShort}"
+		echo "User chose the prefix \"$dept\"."
+		echo "Current computer name is \"$currentName\"."
+		echo "Renaming to \"$deptName\"."
+	    rename_Device "$deptName" # rename device function
+	    exit 0
 	fi
-	deptName="${dept}${serialShort}"
-	echo "User chose the prefix \"$dept\"."
-	echo "Current computer name is \"$currentName\"."
-	echo "Renaming to \"$deptName\"."
-    rename_Device "$deptName" # rename the device
 }
 
-# Function to ask the user if they want to rename the device
+# Seperate dialog list for lab devices
+function lab_Prompt(){
+	echo "User chose \"Lab Device\""
+	labList=$(osascript <<YOO
+		set dropdownResult to choose from list \
+		{"103 - Macelwane", "202 - Xavier", "207 - Xavier", "220 - Xavier", "236 - Xavier"}\
+		with title "SLU ITS: Device Rename" with prompt "Please choose the Lab in which this device will be located:"
+		return dropdownResult
+YOO
+	)
+	lab=$(echo "$labList")
+	labPrefix=$(echo "$lab" | sed 's/\(.*\) - .*/\1/' | sed 's/[[:space:]]*$//')
+	# Cancels the operation if the user selects Cancel from the osascript prompt
+	if [[ "$lab" == *"false"* ]];
+	then
+		echo "User canceled the operation."
+		exit 1
+	# Name appropriately for Macelwane
+	elif [[ "$lab" == *"Macelwane"* ]];
+	then
+		labName="MWH-${labPrefix}-$serialShort"
+		echo "User chose \"Macelwane lab $labPrefix\""
+		echo "Renaming device to \"$labName\""
+		rename_Device "$labName"
+		exit 0
+	# Name appropriately for Xavier
+	elif [[ "$lab" == *"Xavier"* ]];
+	then
+		labName="XVH-${labPrefix}-$serialShort"
+		echo "User chose \"Xavier lab $labPrefix\""
+		echo "Renaming device to \"$labName\""
+		rename_Device "$labName"
+		exit 0
+	# Just in case
+	else
+		echo "Errored out"
+		exit 1
+	fi
+}
+
+# Ask the user if they want to rename the device
 function rename_Ask() {
 	renamePrompt=$(osascript <<OOP
 	    set dialogResult to display dialog "This device name already contains a department prefix, would you like to choose a new one?\n\nCurrent Name: $currentName \n\nSelect \"Yes\" to continue, or \"Cancel\" to use the existing prefix." buttons {"Yes", "Cancel"} default button "Cancel" with title "SLU ITS: Device Rename" giving up after 300
@@ -60,7 +120,7 @@ function rename_Ask() {
 	    	return "Dialog timed out"
 	    end if
 OOP
-)
+	)
 	echo "$renamePrompt"
 }
 
@@ -68,20 +128,18 @@ OOP
 # prompt the user to choose their department prefix.
 if [[ $currentName == *"Mac"* ]];
 then
-	department_Prompt
-	exit 0
+	department_Prompt # department prompt function
 # If the current device name already contains two hyphens,
-# prompt the user if they want to choose a new prefix,
+# prompt the user to select a new prefix,
 # if so, prompt the user to choose their department prefix.
 elif [[ $currentName == *-*-* ]];
 then
 	longPrefix=$(echo "$currentName" | sed 's/\(.*-\).*$/\1/')
 	newLongName="${longPrefix}${serialShort}"
-	renameAnswer=$(rename_Ask) # GUI dialog for the user
+	renameAnswer=$(rename_Ask) # rename prompt function
 	if [[ $renameAnswer == *"Yes"* ]];
 	then
-		department_Prompt
-		exit 0
+		department_Prompt # department prompt function
 	else
 		echo "Current computer name contains hyphens, \"$currentName\" with prefix \"$longPrefix\"."
 		if [[ $currentName == $newLongName ]];
@@ -91,7 +149,7 @@ then
 			exit 0
 		else
 			echo "Renaming to \"$newLongName\"."
-			rename_Device "$newLongName"
+			rename_Device "$newLongName" # rename device function
 			exit 0
 		fi
 	fi
@@ -103,11 +161,10 @@ elif [[ $currentName == *"-"* ]];
 then
 	prefix=$(echo "$currentName" | sed 's/\(.*-\).*/\1/')
 	newName="${prefix}${serialShort}"
-	renameAnswer=$(rename_Ask) # GUI dialog for the user
+	renameAnswer=$(rename_Ask) # rename prompt function
 	if [[ $renameAnswer == *"Yes"* ]];
 	then
-		department_Prompt
-		exit 0
+		department_Prompt # department prompt function
 	else
 		echo "Current computer name contains a hyphen, \"$currentName\" with prefix \"$prefix\"."
 		if [[ $currentName == $newName ]];
@@ -117,7 +174,7 @@ then
 			exit 0
 		else
 			echo "Renaming to \"$newName\"."
-			rename_Device "$newName"
+			rename_Device "$newName" # rename device function
 			exit 0
 		fi
 	fi
@@ -125,6 +182,5 @@ then
 # If the current device name fails to match any conditions,
 # prompt the user to choose their department prefix.
 else
-	department_Prompt
-	exit 0
+	department_Prompt # department prompt function
 fi
