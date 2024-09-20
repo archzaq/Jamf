ceb30a7e536896aaf559f78c226e058c6b146d13
Zaq <129307974+archzaq@users.noreply.github.com>
2024-09-20 13:16:03 -0500

    wip

--- Applications/CyberArk/CyberArk2-ElectricBoogaloo.sh
+++ Applications/CyberArk/CyberArk2-ElectricBoogaloo.sh
@@ -3,8 +3,8 @@
 ##########################
 ### Author: Zac Reeves ###
 ### Created: 6-6-24    ###
-### Updated: 6-27-24   ###
-### Version: 2.3       ###
+### Updated: 9-20-24   ###
+### Version: 2.4       ###
 ##########################
 
 managementAccount="$4"
@@ -12,54 +12,55 @@ managementAccountPass="$5"
 managementAccountPath="/Users/$managementAccount"
 tempAccount="$6"
 tempAccountPassword="$7"
-loggedInUser="$(/usr/bin/defaults read /Library/Preferences/com.apple.loginwindow lastUserName)"
-loggedInUserPassword=''
-title="SLU ITS: CyberArk Installation"
-icon="/usr/local/jamfconnect/SLU.icns"
+#loggedInUser="$(/usr/bin/defaults read /Library/Preferences/com.apple.loginwindow lastUserName)"
+#loggedInUserPassword=''
+currentUserPassword=''
+readonly currentUser="$(/usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/  { print $3 }')"
+readonly logPath='/var/log/cyberarkFix.log'
+readonly iconPath="/usr/local/jamfconnect/SLU.icns"
+readonly dialogTitle="SLU ITS: CyberArk Installation"
 
-# Check if someone is logged into the device
-function login_Check() {
-    local account="$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/  { print $3 }')"
-
-    if [[ "$account" == 'root' ]];
-    then
-        echo "Log: \"$account\" currently logged in"
-        return 1
-    elif [[ "$account" == 'loginwindow' ]] || [[ -z "$account" ]];
-    then
-        echo "Log: No one logged in"
-        return 1
-    else
-        echo "Log: \"$account\" currently logged in"
-        return 0
-    fi
-}
-
-# Check for SLU icon, otherwise the osascript calls will fail
+# Check for SLU icon file, applescript dialog boxes will error without it
 function icon_Check() {
-    if [[ ! -f "$icon" ]];
+    if [[ ! -f "$iconPath" ]];
     then
-        /usr/local/bin/jamf policy
-        if [[ ! -f "$icon" ]];
+        echo "Log: $(date "+%F %T") No SLU icon found, attempting install." | tee -a "$logPath"
+        /usr/local/bin/jamf policy -event SLUFonts
+        if [[ ! -f "$iconPath" ]];
         then
-            echo "Log: No SLU icon installed, exiting."
+            echo "Log: $(date "+%F %T") No SLU icon found, exiting." | tee -a "$logPath"
             return 1
         fi
     fi
-
     return 0
+}
+
+# Check if someone is logged into the device
+function login_Check() {
+    local account="$(/usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/  { print $3 }')"
+    if [[ "$account" == 'root' ]];
+    then
+        echo "Log: $(date "+%F %T") \"$account\" currently logged in." | tee -a "$logPath"
+        return 1
+    elif [[ "$account" == 'loginwindow' ]] || [[ -z "$account" ]];
+    then
+        echo "Log: $(date "+%F %T") No one logged in." | tee -a "$logPath"
+        return 1
+    else
+        echo "Log: $(date "+%F %T") \"$account\" currently logged in." | tee -a "$logPath"
+        return 0
+    fi
 }
 
 # Check if account exists
 function account_Check() {
     local account="$1"
-
-    if id "$account" &>/dev/null;
+    if /usr/bin/id "$account" &>/dev/null;
     then
-        echo "Log: \"$account\" exists"
+        echo "Log: $(date "+%F %T") \"$account\" exists." | tee -a "$logPath"
         return 0
     else
-        echo "Log: \"$account\" does not exist"
+        echo "Log: $(date "+%F %T") \"$account\" does not exist." | tee -a "$logPath"
         return 1
     fi
 }
@@ -68,13 +69,12 @@ function account_Check() {
 function admin_Check(){
     local account="$1"
     local groupList=$(/usr/bin/groups "$account")
-
     if [[ $groupList == *" admin "* ]];
     then
-        echo "Log: \"$account\" is an admin"
+        echo "Log: $(date "+%F %T") \"$account\" is an admin." | tee -a "$logPath"
         return 0
     else
-        echo "Log: \"$account\" is not an admin"
+        echo "Log: $(date "+%F %T") \"$account\" is not an admin." | tee -a "$logPath"
         return 1
     fi
 }
@@ -108,75 +108,81 @@ function secure_Token_Check() {
     fi
 }
 
-# Prompt the user for their password, reprompting if they enter nothing or the dialog times out
+# Prompt the user for their password, reprompting if they enter nothing
 function password_Prompt(){
-    local passTitle="SLU ITS: Password Prompt"
-    echo "Log: Prompting for password"
-
-    loggedInUserPassword=$(/usr/bin/osascript <<OOP
-    set loggedInUserPassword to (display dialog "Please enter your computer password to continue with the installation:" buttons {"OK"} default button "OK" with hidden answer default answer "" with icon POSIX file "$icon" with title "$passTitle" giving up after 900)
-    if button returned of loggedInUserPassword is equal to "OK" then
-        return text returned of loggedInUserPassword
-    else
-        return "timeout"
-    end if
+    local phrase="$1"
+    local readonly passwordTitle='SLU ITS: Password Prompt'
+    echo "Log: $(date "+%F %T") Prompting user for their password." | tee -a "$logPath"
+    currentUserPassword=$(/usr/bin/osascript <<OOP
+        set currentUserPassword to (display dialog "$phrase" buttons {"Cancel", "OK"} default button "OK" with hidden answer default answer "" with icon POSIX file "$iconPath" with title "$passwordTitle" giving up after 900)
+        if button returned of currentUserPassword is equal to "OK" then
+            return text returned of currentUserPassword
+        else
+            return "timeout"
+        end if
 OOP
-	)
-    lowerPass=$(echo "$loggedInUserPassword" | tr '[:upper:]' '[:lower:]')
-    if [[ -z "$loggedInUserPassword" ]];
+    )
+    if [[ $? != 0 ]];
     then
-        echo "Log: No password entered"
-        /usr/bin/osascript -e "display dialog \"Error! You did not enter a password. Please try again.\" buttons {\"OK\"} default button \"OK\" with icon POSIX file \"$icon\" with title \"$passTitle\""
-        password_Prompt
-    elif [[ "$lowerPass" == 'cancel' ]];
-    then
-        echo "Log: User used hidden exit"
+        echo "Log: $(date "+%F %T") User selected cancel." | tee -a "$logPath"
         return 1
-    elif [[ "$loggedInUserPassword" == 'timeout' ]];
+    elif [[ -z "$currentUserPassword" ]];
     then
-        echo "Log: Timed out"
-        password_Prompt
+        echo "Log: $(date "+%F %T") No password entered." | tee -a "$logPath"
+        /usr/bin/osascript -e "display dialog \"Error! You did not enter a password. Please try again.\" buttons {\"OK\"} default button \"OK\" with icon POSIX file \"$iconPath\" with title \"$passwordTitle\""
+        password_Prompt "$phrase"
+    elif [[ "$currentUserPassword" == 'timeout' ]];
+    then
+        echo "Log: $(date "+%F %T") Timed out." | tee -a "$logPath"
+        password_Prompt "$phrase"
     fi
-
+    echo "Log: $(date "+%F %T") Password prompt finished." | tee -a "$logPath"
     return 0
 }
 
-# Assigns secure token to management account
+# Assigns secure token to current user using management account
 function assign_Token(){
     # Test the sysadminctl command for a success before actually attempting to grant a secure token
-    output=$(/usr/sbin/sysadminctl -adminUser "$loggedInUser" -adminPassword "$loggedInUserPassword" -secureTokenOn "$managementAccount" -password "$managementAccountPass" -test 2>&1)
-
-    # If the test was successful, assign management account a secure token
-    if [[ $output == *"Done"* ]];
+    local secureTokenTest=$(/usr/sbin/sysadminctl -adminUser "$managementAccount" -adminPassword "$managementAccountPass" -secureTokenOn "$currentUser" -password "$currentUserPassword" -test 2>&1)
+    if [[ $secureTokenTest == *"Done"* ]];
     then
-        /usr/sbin/sysadminctl -adminUser "$loggedInUser" -adminPassword "$loggedInUserPassword" -secureTokenOn "$managementAccount" -password "$managementAccountPass"
-        echo "Log: Success!!"
+        /usr/sbin/sysadminctl -adminUser "$managementAccount" -adminPassword "$managementAccountPass" -secureTokenOn "$currentUser" -password "$currentUserPassword"
+        echo "Log: $(date "+%F %T") Success assigning secure token!" | tee -a "$logPath"
         return 0
-
-    # If the test was not successful, exit
     else
-        echo "Log: Error with sysadminctl command"
+        echo "Log: $(date "+%F %T") Error with sysadminctl command." | tee -a "$logPath"
         return 1
     fi
 }
 
-# Creates management account with a home folder with proper permissions
-function create_ManagementAccount() {
-    /usr/sbin/sysadminctl -addUser "$managementAccount" -password "$managementAccountPass" -home "$managementAccountPath" -admin -adminUser "$tempAccount" -adminPassword "$tempAccountPassword"
-    if account_Check "$managementAccount";
+# Creates account with a home folder and proper permissions
+function create_Account() {
+    local accountAdd="$1"
+    local accountAddPass="$2"
+    local accountAddPath="$3"
+    local managementAccount="$4"
+    local managementAccountPass="$5"
+    if /usr/sbin/sysadminctl -addUser "$accountAdd" -password "$accountAddPass" -home "$accountAddPath" -admin -adminUser "$managementAccount" -adminPassword "$managementAccountPass";
     then
-        dirs=("Desktop" "Documents" "Downloads" "Library" "Movies" "Music" "Pictures" "Public")
-        for dir in "${dirs[@]}";
-        do
-            mkdir -p "$managementAccountPath/$dir"
-        done
-        chown -R "$managementAccount":staff "$managementAccountPath"
-        chmod -R 750 "$managementAccountPath"
-        chmod -R +a "group:everyone deny delete" "$managementAccountPath"
-        echo "Log: \"$managementAccount\" successfully created"
-        return 0
+        echo "Log: $(date "+%F %T") \"$accountAdd\" created." | tee -a "$logPath"
+        if account_Check "$accountAdd";
+        then
+            dirs=("Desktop" "Documents" "Downloads" "Library" "Movies" "Music" "Pictures" "Public")
+            for dir in "${dirs[@]}";
+            do
+                mkdir -p "$accountAddPath/$dir"
+            done
+            chown -R "$accountAdd":staff "$accountAddPath"
+            chmod -R 750 "$accountAddPath"
+            chmod -R +a "group:everyone deny delete" "$accountAddPath"
+            echo "Log: $(date "+%F %T") \"$accountAdd\" successfully configured." | tee -a "$logPath"
+            return 0
+        else
+            echo "Log: $(date "+%F %T") \"$accountAdd\" failed to be configured." | tee -a "$logPath"
+            return 1
+        fi
     else
-        echo "Log: \"$managementAccount\" failed to be created"
+        echo "Log: $(date "+%F %T") \"$accountAdd\" could not be created." | tee -a "$logPath"
         return 1
     fi
 }
@@ -203,24 +209,28 @@ function gather_SecureToken_UserList() {
     fi
 }
 
-# Check current user for volume ownership
-function check_LoggedInUser_Ownership() {
-    loggedInUserGUID=$(/usr/bin/dscl . -read /Users/$loggedInUser GeneratedUID | awk '{print $2}')
-    systemVolume=$(/usr/sbin/diskutil info / | grep "Volume Name" | sed 's/.*: //' | sed 's/^ *//')
-    systemVolumePath="/Volumes/$systemVolume"
-
+# Check user account for volume ownership
+function check_Ownership() {
+    local account="$1"
+    local volumeOwner=false
+    local accountGUID=$(/usr/bin/dscl . -read /Users/$account GeneratedUID | awk '{print $2}')
+    local systemVolume=$(/usr/sbin/diskutil info / | grep "Volume Name" | sed 's/.*: //' | sed 's/^ *//')
+    local systemVolumePath="/Volumes/$systemVolume"
+    echo "Log: $(date "+%F %T") Checking \"$account\" for volume ownership." | tee -a "$logPath"
     for id in $(/usr/sbin/diskutil apfs listUsers "$systemVolumePath" | grep -E '.*-.*' | awk '{print $2}');
     do
-        if [[ "$id" == "$loggedInUserGUID" ]];
+        if [[ "$id" == "$accountGUID" ]];
         then
-            echo "Log: \"$loggedInUser\" has volume ownership"
+            echo "Log: $(date "+%F %T") \"$account\" is a volume owner." | tee -a "$logPath"
+            volumeOwner=true
             return 0
         fi
     done
-
-    echo "Log: \"$loggedInUser\" does not have volume ownership"
-    /usr/bin/osascript -e "display dialog \"You are not a volume owner! Your account does not have the proper permission to continue.\n\n$phrase\" buttons {\"OK\"} default button \"OK\" with icon POSIX file \"$icon\" with title \"$title\""
-    return 1
+    if [[ "$volumeOwner" != true ]];
+    then
+        echo "Log: $(date "+%F %T") \"$account\" is not a volume owner." | tee -a "$logPath"
+        return 1
+    fi
 }
 
 # Dialog box to inform user of the overall process taking place
@@ -265,45 +275,72 @@ function final_Check() {
     fi
 }
 
+# Check to delete temporary account before exiting
+function exitError() {
+    if account_Check "$tempAccount";
+    then
+        /usr/sbin/sysadminctl -deleteUser "$tempAccount" -secure
+    fi
+    exit 1
+}
+
 function main() {
     promptCounter=0
+    echo "Log: $(date "+%F %T") Beginning CyberArk fix script." | tee "$logPath"
 
-    # Ensure someone is logged into the GUI before running 
-    if ! login_Check;
-    then
-        /usr/sbin/sysadminctl -deleteUser "$tempAccount" -secure
-        exit 1
-    fi
-
-    # Ensure SLU icon is present
+    # Check for SLU icon file
+    echo "Log: $(date "+%F %T") Checking for SLU icon." | tee -a "$logPath"
     if ! icon_Check;
     then
-        /usr/sbin/sysadminctl -deleteUser "$tempAccount" -secure
-        exit 1
+        echo "Log: $(date "+%F %T") Exiting for no SLU icon." | tee -a "$logPath"
+        exitError
     fi
+    echo "Log: $(date "+%F %T") Check for SLU icon complete." | tee -a "$logPath"
 
-    # Ensure temp account is present
+
+
+    # Check for valid user being logged in
+    echo "Log: $(date "+%F %T") Checking for currently logged in user." | tee -a "$logPath"
+    if ! login_Check;
+    then
+        echo "Log: $(date "+%F %T")Exiting for invalid user logged in." | tee -a "$logPath"
+        exitError
+    fi
+    echo "Log: $(date "+%F %T") Check for currently logged in user complete." | tee -a "$logPath"
+
+
+
+    # Check if temp account exists
+    echo "Log: $(date "+%F %T") Checking for temporary account." | tee -a "$logPath"
     if ! account_Check "$tempAccount";
     then
-        /usr/sbin/sysadminctl -deleteUser "$tempAccount" -secure
-        exit 1
+        echo "Log: $(date "+%F %T")Temporary account does not exist, exiting." | tee -a "$logPath"
+        exitError
     fi
+    echo "Log: $(date "+%F %T") Check for temporary account complete." | tee -a "$logPath"
+
+
+
+    # Check if management account exists, create it if not
+    echo "Log: $(date "+%F %T") Checking for \"$managementAccount\"." | tee -a "$logPath"
+    if ! account_Check "$managementAccount";
+    then
+        if ! create_Account "$managementAccount" "$managementAccountPass" "$managementAccountPath" "$tempAccount" "$tempAccountPassword";
+        then
+            echo "Log: $(date "+%F %T") Exiting at account creation." | tee -a "$logPath"
+            /usr/bin/osascript -e 'display alert "An error has occurred" message "There was an issue with creating management account. Unable to proceed." as critical buttons {"OK"} default button "OK" giving up after 900'
+            exitError
+        fi
+        echo "Log: $(date "+%F %T") Creation of \"$managementAccount\" complete." | tee -a "$logPath"
+    fi
+    echo "Log: $(date "+%F %T") Check for \"$managementAccount\" complete." | tee -a "$logPath"
+
+
 
     # Prompt user with the action to take place
     if ! user_Prompt;
     then
-        /usr/sbin/sysadminctl -deleteUser "$tempAccount" -secure
-        exit 1
-    fi
-
-    # If management account does not exist, create it
-    if ! account_Check "$managementAccount";
-    then
-        if ! create_ManagementAccount;
-        then
-            /usr/sbin/sysadminctl -deleteUser "$tempAccount" -secure
-            exit 1
-        fi
+        exitError
     fi
 
     # If management account is not an admin, assign it to admin group using temp account credentials
@@ -311,8 +348,7 @@ function main() {
     then
         if ! add_Account_To_AdminGroup "$managementAccount";
         then
-            /usr/sbin/sysadminctl -deleteUser "$tempAccount" -secure
-            exit 1
+            exitError
         fi
     fi
 
@@ -320,8 +356,7 @@ function main() {
     gather_SecureToken_UserList
     if ! check_LoggedInUser_Ownership;
     then
-        /usr/sbin/sysadminctl -deleteUser "$tempAccount" -secure
-        exit 1
+        exitError
     fi
 
     # If management account doesnt have a secure token, ask for their password, then grant admin only to assign a token
@@ -331,8 +366,7 @@ function main() {
         then
             if ! password_Prompt;
             then
-                /usr/sbin/sysadminctl -deleteUser "$tempAccount" -secure
-                exit 1
+                exitError
             fi
 
             if add_Account_To_AdminGroup "$loggedInUser";
@@ -341,14 +375,12 @@ function main() {
                 /usr/sbin/dseditgroup -o edit -d "$loggedInUser" -u "$tempAccount" -P "$tempAccountPassword" -t user -L admin
             else
                 echo "Log: Failed to add \"$loggedInUser\" to admin group"
-                /usr/sbin/sysadminctl -deleteUser "$tempAccount" -secure
-                exit 1
+                exitError
             fi
         else
             if ! password_Prompt;
             then
-                /usr/sbin/sysadminctl -deleteUser "$tempAccount" -secure
-                exit 1
+                exitError
             fi
             assign_Token
         fi
