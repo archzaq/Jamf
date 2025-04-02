6267e878fdd4d224abddae1dd803b15ef5136cbd
Zachary Reeves <129307974+archzaq@users.noreply.github.com>
2025-04-02 13:10:45 -0500

    reorg

--- Maintenance/menuBar_Spacing.sh
+++ Maintenance/menuBar_Spacing.sh
@@ -4,7 +4,7 @@
 ### Author: Zac Reeves ###
 ### Created: 4-2-25    ###
 ### Updated: 4-2-25    ###
-### Version: 1.0       ###
+### Version: 1.1       ###
 ##########################
 
 readonly logPath='/var/log/menuBar_Spacing.log'
@@ -22,16 +22,18 @@ function main() {
         if /usr/bin/defaults -currentHost write -globalDomain NSStatusItemSelectionPadding -int 8;
         then
             log_Message "Successfully set Menu Bar icon selection spacing."
+            log_Message "Exiting!"
+            exit 0
         else
             log_Message "Unable to set Menu Bar icon selection spacing."
         fi
     else
         log_Message "Unable to set Menu Bar icon spacing."
-        log_Message "Exiting without making any changes."
-        exit 1
     fi
-    log_Message "Exiting!"
-    exit 0
+    log_Message "Exiting without making any changes."
+    /usr/bin/defaults -currentHost delete -globalDomain NSStatusItemSpacing
+    /usr/bin/defaults -currentHost delete -globalDomain NSStatusItemSelectionPadding
+    exit 1
 }
 
 main
