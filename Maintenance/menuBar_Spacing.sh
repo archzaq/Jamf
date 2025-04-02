e4f501719ccfa8dbf909961ca98a28e6965467d5
Zachary Reeves <129307974+archzaq@users.noreply.github.com>
2025-04-02 15:27:41 -0500

    permissions for deleting

--- Maintenance/menuBar_Spacing.sh
+++ Maintenance/menuBar_Spacing.sh
@@ -4,7 +4,7 @@
 ### Author: Zac Reeves ###
 ### Created: 4-2-25    ###
 ### Updated: 4-2-25    ###
-### Version: 1.3       ###
+### Version: 1.4       ###
 ##########################
 
 readonly logPath='/var/log/menuBar_Spacing.log'
@@ -35,11 +35,11 @@ function main() {
         exit 1
     fi
     log_Message "Setting Menu Bar icon spacing."
-    if $(su "$account" -c "/usr/bin/defaults -currentHost write -globalDomain NSStatusItemSpacing -int 10");
+    if su "$account" -c "/usr/bin/defaults -currentHost write -globalDomain NSStatusItemSpacing -int 10";
     then
         log_Message "Successfully set Menu Bar icon spacing."
         log_Message "Setting Menu Bar icon selection spacing."
-        if $(su "$account" -c "/usr/bin/defaults -currentHost write -globalDomain NSStatusItemSelectionPadding -int 8");
+        if su "$account" -c "/usr/bin/defaults -currentHost write -globalDomain NSStatusItemSelectionPadding -int 8";
         then
             log_Message "Successfully set Menu Bar icon selection spacing."
             log_Message "Exiting!"
@@ -51,8 +51,8 @@ function main() {
         log_Message "Unable to set Menu Bar icon spacing."
     fi
     log_Message "Exiting and reverting changes."
-    /usr/bin/defaults -currentHost delete -globalDomain NSStatusItemSpacing
-    /usr/bin/defaults -currentHost delete -globalDomain NSStatusItemSelectionPadding
+    su "$account" -c "/usr/bin/defaults -currentHost delete -globalDomain NSStatusItemSpacing"
+    su "$account" -c "/usr/bin/defaults -currentHost delete -globalDomain NSStatusItemSelectionPadding"
     exit 1
 }
 
