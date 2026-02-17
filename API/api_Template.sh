a6b49c8431c3d024d31cc36b2ba939a5408a415a
archzaq <129307974+archzaq@users.noreply.github.com>
2026-02-17 11:02:11 -0600

    updated logic

--- API/api_Template.sh
+++ API/api_Template.sh
@@ -4,36 +4,47 @@
 ############################
 ###  Author:  Zac Reeves ###
 ###  Created: 02-16-26   ###
-###  Updated: 02-16-26   ###
-###  Version: 0.1        ###
+###  Updated: 02-17-26   ###
+###  Version: 0.2        ###
 ############################
 
 readonly currentUser="$(/usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/  { print $3 }')"
+jamfURL="$(security find-generic-password -s "jamf-api-url" -a "$currentUser" -w)"
+clientID="$(security find-generic-password -s "jamf-api-id" -a "$currentUser" -w)"
+clientSecret="$(security find-generic-password -s "jamf-api-sec" -a "$currentUser" -w)"
 
-# Authenticate
-jamfURL=$(security find-generic-password -s "jamf-api-url" -a "$currentUser" -w)
-clientID=$(security find-generic-password -s "jamf-api-id" -a "$currentUser" -w)
-clientSecret=$(security find-generic-password -s "jamf-api-sec" -a "$currentUser" -w)
+if [[ -z "$jamfURL" ]] || [[ -z "$clientID" ]] || [[ -z "$clientSecret" ]];
+then
+    printf "Missing critical arguments\n"
+    exit 1
+fi
+
 accessToken=$(curl --silent --location \
-	--request POST "https://${jamfURL}/api/oauth/token" \
+	--request POST "${jamfURL}/api/oauth/token" \
 	--header "Content-Type: application/x-www-form-urlencoded" \
 	--data-urlencode "client_id=${clientID}" \
 	--data-urlencode "grant_type=client_credentials" \
 	--data-urlencode "client_secret=${clientSecret}" | plutil -extract "access_token" raw -o - -)
 
-# Make GET call to Jamf Pro information
-response=$(curl -s -o /dev/null -w "%{http_code}" \
-    -H "Accept: application/json" \
-    -H "Authorization: Bearer $accessToken" \
-    -X GET "https://${jamfURL}/api/v2/jamf-pro-information")
-
-if [[ "$response" -eq 200 ]];
+if [[ -z "$accessToken" ]];
 then
-    printf "It worked!\n"
-else
-    printf "Failed with HTTP %s\n" "$response"
+    printf "Missing access token\n"
     exit 1
 fi
 
+response=$(curl -s -o /dev/null -w "%{http_code}" \
+    -H "Accept: application/json" \
+    -H "Authorization: Bearer $accessToken" \
+    -X GET "${jamfURL}/api/v2/jamf-pro-information")
+
+if [[ "$response" == "200" ]] || [[ "$response" -eq 200 ]];
+then
+    printf "Valid Bearer Token\n"
+else
+    printf "Failed: %s\n" "$response"
+    exit 1
+fi
+
+exit 0
 
 
