44fe85a4c5ca33af77dbbfd2a6100d10decef877
archzaq <129307974+archzaq@users.noreply.github.com>
2026-02-16 23:31:05 -0600

    added folders

--- API/Local/os_checkStatus.sh
+++ API/Local/os_checkStatus.sh
@@ -1,39 +1,33 @@
-
 #!/bin/bash
 
 ############################
 ###  Author:  Zac Reeves ###
 ###  Created: 02-16-26   ###
 ###  Updated: 02-16-26   ###
-###  Version: 0.1        ###
+###  Version: 1.0        ###
 ############################
 
 readonly currentUser="$(/usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/  { print $3 }')"
+planUUID="$1"
+
+if [[ -z "$planUUID" ]];
+then
+    printf "Missing Plan UUID as argument\n"
+    exit 1
+fi
 
 # Authenticate
-jamfURL=$(security find-generic-password -s "jamf-api-url" -a "$currentUser" -w)
+jamfURL="https://$(security find-generic-password -s "jamf-api-url" -a "$currentUser" -w)"
 clientID=$(security find-generic-password -s "jamf-api-id" -a "$currentUser" -w)
 clientSecret=$(security find-generic-password -s "jamf-api-sec" -a "$currentUser" -w)
 accessToken=$(curl --silent --location \
-	--request POST "https://${jamfURL}/api/oauth/token" \
+	--request POST "${jamfURL}/api/oauth/token" \
 	--header "Content-Type: application/x-www-form-urlencoded" \
 	--data-urlencode "client_id=${clientID}" \
 	--data-urlencode "grant_type=client_credentials" \
 	--data-urlencode "client_secret=${clientSecret}" | plutil -extract "access_token" raw -o - -)
 
-# Make GET call to Jamf Pro information
-response=$(curl -s -o /dev/null -w "%{http_code}" \
-    -H "Accept: application/json" \
+curl -s -H "Accept: application/json" \
     -H "Authorization: Bearer $accessToken" \
-    -X GET "https://${jamfURL}/api/v2/jamf-pro-information")
-
-if [[ "$response" -eq 200 ]];
-then
-    printf "It worked!\n"
-else
-    printf "Failed with HTTP %s\n" "$response"
-    exit 1
-fi
-
-
+    -X GET "${jamfURL}/api/v1/managed-software-updates/plans/${planUUID}"
 
